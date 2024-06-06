80f05810666f95c6b7921e8ebcf8d355b2011659
vergilx6x <med.work96@hotmail.com>
2024-06-06 15:14:12 -0700

    fix

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,19 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Travers the three in-orderr.
+ * binary_tree_inorder - Travers the three in-order.
  *
  * @tree: A pointer to the root of the treei.
  * @func: A pointer to the function to call on each node.
  *
  */
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		binary_tree_preorder(tree->left, func);
+		binary_tree_inorder(tree->left, func);
 		func(tree->n);
-		binary_tree_preorder(tree->right, func);
+		binary_tree_inorder(tree->right, func);
 	}
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Travers the three in a pre-order.
+ * binary_tree_postorder - Travers the three in a post-order.
  *
  * @tree: A pointer to the root of the treei.
  * @func: A pointer to the function to call on each node.
@@ -9,12 +9,12 @@
  * Return: 1 if the node is a leaf or 0 if not.
  */
 
-void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		binary_tree_preorder(tree->left, func);
-		binary_tree_preorder(tree->right, func);
+		binary_tree_postorder(tree->left, func);
+		binary_tree_postorder(tree->right, func);
 		func(tree->n);
 	}
 }
