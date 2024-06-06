8cac2b5f7ba76d580b32315a18a4b3931799f057
vergilx6x <med.work96@hotmail.com>
2024-06-06 15:11:13 -0700

    fix

--- 7-binary_tree_inorder.c
+++ 7-binary_tree_inorder.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_preorder - Travers the three in a pre-order.
+ * binary_tree_preorder - Travers the three in-orderr.
  *
  * @tree: A pointer to the root of the treei.
  * @func: A pointer to the function to call on each node.
@@ -12,8 +12,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		func(tree->n);
 		binary_tree_preorder(tree->left, func);
+		func(tree->n);
 		binary_tree_preorder(tree->right, func);
 	}
 }
--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,14 +6,15 @@
  * @tree: A pointer to the root of the treei.
  * @func: A pointer to the function to call on each node.
  *
+ * Return: 1 if the node is a leaf or 0 if not.
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
 	if (tree && func)
 	{
-		func(tree->n);
 		binary_tree_preorder(tree->left, func);
 		binary_tree_preorder(tree->right, func);
+		func(tree->n);
 	}
 }
