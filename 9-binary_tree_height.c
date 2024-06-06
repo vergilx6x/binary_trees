b57b83e9c19e8e24250b1751572aa423de80ea66
vergilx6x <med.work96@hotmail.com>
2024-06-06 15:19:31 -0700

    fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,12 +12,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		size_t left_height = 0, right_height = 0;
-		
+
 		if (tree->left)
 			left_height = 1 + binary_tree_height(tree->left);
 		if (tree->right)
 			right_height = 1 + binary_tree_height(tree->right);
-		
+
 		return ((left_height > right_height) ? left_height : right_height);
 	}
 
